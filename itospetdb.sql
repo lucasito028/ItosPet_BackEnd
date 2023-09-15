c40672df46e5e9be1c353133870623ae643f9b04
lucasito028 <lucastoshihiroito28@gmail.com>
2023-09-14 23:01:17 -0300

    Pedlo

--- itospetdb.sql
+++ itospetdb.sql
@@ -15,120 +15,119 @@ create table TIPOPESSOA(
 
 
 create table PESSOA(
-CPF int not null auto_increment,
-FKIDTIPOPESSOA int not null,
+    CPF int not null auto_increment,
+    FKTPESSOA int not null,
 
-NOME varchar(45) not null,
-SOBRENOME varchar(45) not null,
+    NOME varchar(45) not null,
+    SOBRENOME varchar(45) not null,
 
-primary key(CPF),
-foreign key(FKIDTIPOPESSOA) references TIPOPESSOA(IDTIPOPESSOA),
+    primary key(CPF),
+    foreign key(FKIDTIPOPESSOA) references TIPOPESSOA(IDTIPOPESSOA),
 
-index NOMECOMPLETO (NOME, SOBRENOME)
+    index NOMECOMPLETO (NOME, SOBRENOME)
 );
 
 
 
 
 create table TIPOANIMAL(
-IDTIPO int not null auto_increment,
+    IDTIPO int not null auto_increment,
 
-NOMETIPO varchar(45) not null,
+    NOMETIPO varchar(45) not null,
 
-primary key(IDTIPO)
+    primary key(IDTIPO)
 );
 
 
 create table RACA(
-IDRACA int not null auto_increment,
-TIPO_ID int not null,
+    IDRACA int not null auto_increment,
+    FKTIPOANIMAL int not null,
 
-NOMERACA varchar(45) not null,
+    NOMERACA varchar(45) not null,
 
-primary key(IDRACA),
-foreign key(TIPO_ID) references TIPOANIMAL(IDTIPO)
+    primary key(IDRACA),
+    foreign key(FKTIPOANIMAL) references TIPOANIMAL(IDTIPO)
 );
 
 
 
 create table PET(
-IDPET int not null auto_increment,
-CPFPESSOA int not null,
-RACA_ID int not null,
+    IDPET int not null auto_increment,
+    FKRACA int not null,
+    FKCPF int not null,
 
-APELIDO varchar(45) not null,
-IDADE INT not null,
-DATANASC DATE NOT NULL,
+    APELIDO varchar(45) not null,
+    IDADE INT not null,
+    DATANASC DATE NOT NULL,
 
-primary key(IDPET),
-foreign key(CPFPESSOA) references PESSOA(CPF),
-foreign key(RACA_ID) references RACA(IDRACA)
+    primary key(IDPET),
+    foreign key(FKCPF) references PESSOA(CPF),
+    foreign key(FKRACA) references RACA(IDRACA)
 );
 
 
 
 
-create table MEIO_VENDA(
-    IDMEIO_VENDA int not null auto_increment,
+create table ONDE(
+    IDONDE int not null auto_increment,
 
-    NOMEMEIO_VENDA varchar(45) not null,
+    NOMEONDE varchar(45) not null,
 
-    primary key(IDMEIO_VENDA)
+    primary key(IDONDE)
 );
 
 
-create table MEIO_PAGAMENTO(
-    IDMEIO_PAGAMENTO int not null auto_increment,
+create table MEIOPAGAMENTO(
+    IDMEIOPAGAMENTO int not null auto_increment,
 
-    NOMEMEIO_PAGAMENTO varchar(45) not null,
-    PARCELAMEIO_PAGAMENTO int not null,
+    NMEIOPGT varchar(45) not null,
+    PARCELA int not null,
 
-    primary key(IDMEIO_PAGAMENTO)
+    primary key(IDMEIOPAGAMENTO)
 );
 
 
-create table ESTADO_VENDA(
-    IDESTADO_VENDA int not null auto_increment,
+create table ESTADOVENDA(
+    IDESTADOVENDA int not null auto_increment,
 
-    NOMEESTADO_VENDA varchar(45) not null,
+    NOMESTADOVENDA varchar(45) not null,
 
-    primary key(IDESTADO_VENDA)
+    primary key(IDESTADOVENDA)
 );
 
 
 
-CREATE TABLE ESTADOPRODUTO(
-    IDESTADOPRODUTO int NOT NULL auto_increment,
 
-    NOMESTADOPRODUTO varchar(45) not null,
+create table ESTADOPRODUTO(
+    IDESTADOPRODUTO int not null auto_increment,
+
+    NMESTADOPROD varchar(45) not null,
 
     primary key(IDESTADOPRODUTO)
-    );
+);
 
 
 create table TIPOPRODUTO(
-    IDTIPOPROD int NOT NULL auto_increment,
+    IDESTADOPRODUTO int NOT NULL auto_increment,
 
-    NOMETIPOPROD varchar(45) not null,
+    NMESTADOPROD varchar(45) not null,
 
-    primary key(IDTIPOPROD)
+    primary key(IDESTADOPRODUTO)
 );
 
 
 create table PRODUTO(
     IDPRODUTO int not null auto_increment,
-    FK_IDTIPOPROD int not null,
     FKESTADOPRODUTO int not null,
-    FKMEIO_VENDA int not null,
+    FKTIPOPRODUTO int not null,
 
     NOMEPRODUTO varchar(45) not null,
     PRECO double not null,
     QTD int not null, 
 
     primary key(IDPRODUTO),
-    foreign key (FK_IDTIPOPROD) references TIPOPRODUTO(IDTIPOPROD),
-    foreign key (FKESTADOPRODUTO) references ESTADOPRODUTO(IDESTADOPRODUTO),
-    foreign key (FKMEIO_VENDA) references MEIO_VENDA(IDMEIO_VENDA)
+    foreign key (FKTIPOPRODUTO) references TIPOPRODUTO(IDESTADOPRODUTO),
+    foreign key (FKESTADOPRODUTO) references ESTADOPRODUTO(IDESTADOPRODUTO)
 );
 
 
@@ -136,105 +135,117 @@ create table PRODUTO(
 
 create table VENDA(
     IDVENDA int not null auto_increment,
-    FKMEIOVENDA int not null,
-    FKMEIO_PAGAMENTO int not null,
-    FKESTADO_VENDA int not null,
-    FKCPF int not null,
+    FKMEIOPG int not null,
+    FKESTADOVENDA int not null,
+    FKONDE int not null,
+    FKPESSOA int not null,
 
     PRECOTOTAL double not null,
     DTVENDA date not null,
 
     primary key(IDVENDA),
-    foreign key (FKMEIOVENDA) references MEIO_VENDA(IDMEIO_VENDA),
-    foreign key (FKMEIO_PAGAMENTO) references MEIO_PAGAMENTO(IDMEIO_PAGAMENTO),
-    foreign key (FKESTADO_VENDA) references ESTADO_VENDA(IDESTADO_VENDA),
-    foreign key (FKCPF) references PESSOA(CPF)
+    foreign key (FKMEIOPG) references MEIOPAGAMENTO(IDMEIOPAGAMENTO),
+    foreign key (FKESTADOVENDA) references ESTADOVENDA(IDESTADOVENDA),
+    foreign key (FKONDE) references ONDE(IDONDE),
+    foreign key (FKPESSOA) references PESSOA(CPF)
 );
 
 
 
--- Isso é uma conecao de uma chave a outra
+
+create table LOCALPRODUTO(
+    FKONDE int not null,
+    FKPRODUTO int not null,
+
+    foreign key (FKONDE) references ONDE(IDONDE),
+    foreign key (FKPRODUTO) references PRODUTO(IDPRODUTO),
+);
+
+
 create table PETVENDA(
-    PET_IDPET int not null,
-    VENDA_IDVENDA int not null, 
+    FKPET int not null,
+    FKVENDA int not null, 
 
-    foreign key(PET_IDPET) references PET(IDPET),
-    foreign key(VENDA_IDVENDA) references VENDA(IDVENDA)
+    foreign key(FKPET) references PET(IDPET),
+    foreign key(FKVENDA) references VENDA(IDVENDA)
 );
 
 
--- Também é
 create table ITEMVENDA(
-    PRODUTO_IDPRODUTO int not null,
-    FK_IDVENDA int not null,
+    FKVENDA int not null,
+    FKPRODUTO int not null,
+
     QTD_ITEM int not null, 
 
-    foreign key(PRODUTO_IDPRODUTO) references PET(IDPET),
-    foreign key(FK_IDVENDA) references VENDA(IDVENDA)
+    foreign key(FKVENDA) references VENDA(IDVENDA),
+    foreign key(FKPRODUTO) references PRODUTO(IDPRODUTO)
 );
 
 
 
 
 insert into TIPOPESSOA(NOMETIPOPESSOA) values ("Cliente"),
-("Vendedor");
+    ("Vendedor");
 
 
 
-insert into PESSOA(FKIDTIPOPESSOA, NOME, SOBRENOME) values (1, "Pedlo", "Pogo"),
-(1, "Foo", "Kao"),
-(1, "Jao", "Paulo"),
-(1, "Coelho", "PEgador"),
-(2, "Abril", "Inhegas");
+insert into PESSOA(FKTPESSOA, NOME, SOBRENOME) values (1, "Pedlo", "Pogo"),
+    (1, "Foo", "Kao"),
+    (1, "Jao", "Paulo"),
+    (1, "Coelho", "PEgador"),
+    (2, "Abril", "Inhegas");
 
 
 
 insert into TIPOANIMAL (NOMETIPO) values 
-('Cachorro'),
-('Gato'),
-('Peixe');
+    ('Cachorro'),
+    ('Gato'),
+    ('Peixe');
 
 
-insert into RACA (TIPO_ID, NOMERACA) values 
-(1, 'Labrador'),
-(1, 'Poodle'),
-(1, 'Bulldog'),
-(1, 'Vira Lata'),
-(1, 'Pasto Alemões'),
-(1, 'Golden Retriever'),
+insert into RACA (FKTIPOANIMAL, NOMERACA) values 
+    (1, 'Labrador'),
+    (1, 'Poodle'),
+    (1, 'Bulldog'),
+    (1, 'Vira Lata'),
+    (1, 'Pasto Alemões'),
+    (1, 'Golden Retriever'),
 
-(2, 'Mial'),
-(2, 'Abyssinian'),
-(2, 'Aegean'),
-(2, 'Arabian Mau'),
+    (2, 'Mial'),
+    (2, 'Abyssinian'),
+    (2, 'Aegean'),
+    (2, 'Arabian Mau'),
 
-(3, 'Kanpachi'),
-(3, 'Sake'),
-(3, 'Maguro');
-
-
-insert into MEIO_VENDA (NOMEMEIO_VENDA) values 
-('Presencial'),
-('Ecommerce');
-
-insert into MEIO_PAGAMENTO (NOMEMEIO_PAGAMENTO, PARCELAMEIO_PAGAMENTO) values
-('Dinheiro', 1),
-('PIX', 1),
-('Débito', 1),
-('Crédito à Vista', 1),
-('Parcelado 2X', 2),
-('Parcelado 3X', 3),
-('Parcelado 4X', 4),
-('Parcelado 5X', 5),
-('Parcelado 6X', 6);
-
-insert into ESTADO_VENDA(NOMEESTADO_VENDA) values 
-("Finalizado"),
-("Cancelado"),
-("Pendente");
+    (3, 'Kanpachi'),
+    (3, 'Sake'),
+    (3, 'Maguro');
 
 
-insert into TIPOPRODUTO(NOMETIPOPROD) values 
-("Servico"),
-("Produto");
+
+insert into ONDE (NOMEONDE) values 
+    ('Presencial'),
+    ('Ecommerce');
+
+
+insert into MEIOPAGAMENTO (NMEIOPGT, PARCELA) values
+    ('Dinheiro', 1),
+    ('PIX', 1),
+    ('Débito', 1),
+    ('Crédito à Vista', 1),
+    ('Parcelado 2X', 2),
+    ('Parcelado 3X', 3),
+    ('Parcelado 4X', 4),
+    ('Parcelado 5X', 5),
+    ('Parcelado 6X', 6);
+
+
+insert into ESTADOVENDA(NOMESTADOVENDA) values 
+    ("Finalizado"),
+    ("Cancelado"),
+    ("Pendente");
+
+
+insert into TIPOPRODUTO(NMESTADOPROD) values 
+    ("Servico"),
+    ("Produto");
 
